fa989964550c08a0d50cf6595c3994b82fd27924
Jessica Burns <jburns@neboagency.com>
2018-06-14 13:54:45 -0400

    Add user profile addition/update API call

--- lib/braze/users.rb
+++ lib/braze/users.rb
@@ -5,5 +5,11 @@ module Braze
     def self.export_ids(api, email_address)
       api.execute('/users/export/ids', email_address: email_address)
     end
+
+    # POST https://YOUR_REST_API_URL/users/track
+    # https://www.braze.com/documentation/REST_API/#user-track-request
+    def self.upsert_profile(api, attributes)
+      api.execute('/users/track', attributes: [attributes])
+    end
   end
 end
--- spec/users_spec.rb
+++ spec/users_spec.rb
@@ -1,10 +1,24 @@
 require 'spec_helper'
 
 describe Braze::Users do
+  before(:all) do
+    @braze_api = Braze::Api.new(api_key: ENV['BRAZE_API_KEY'], endpoint_url: ENV['BRAZE_ENDPOINT_URL'])
+  end
+
   describe 'Export IDs' do
     it 'returns a json response for a valid request' do
-      braze_api = Braze::Api.new(api_key: ENV['BRAZE_API_KEY'], endpoint_url: ENV['BRAZE_ENDPOINT_URL'])
-      expect(Braze::Users.export_ids(braze_api, ENV['BRAZE_EMAIL'])).to include('message', 'users')
+      expect(Braze::Users.export_ids(@braze_api, ENV['BRAZE_EMAIL'])).to include('message', 'users')
+    end
+  end
+
+  describe 'Upsert Profile' do
+    it 'returns a json response for a valid request' do
+      attributes = {
+        external_id: Digest::SHA256.hexdigest(ENV['BRAZE_EMAIL'].downcase),
+        first_name: 'Test',
+        last_name: 'Test'
+      }
+      expect(Braze::Users.upsert_profile(@braze_api, attributes)['message']).to eq 'success'
     end
   end
 end
