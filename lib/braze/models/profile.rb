b8e6103bd78a546a9a3b66548ba1be143c8111bb
Jessica Burns <jburns@neboagency.com>
2018-06-25 14:53:13 -0400

    Update profile fields
    See FS documentation 6/20

--- lib/braze/models/profile.rb
+++ lib/braze/models/profile.rb
@@ -5,14 +5,14 @@ module Braze
 
     attr_reader :email, :external_id, :email_hash, :first_name, :last_name
     attr_accessor :created, :device_signup, :email_subscribe, :newsletter_signup,
-                  :newsletter_updated, :ps4_notification, :subscription_status,
-                  :system_of_origin, :updated, :university_name,
-                  :xbox_notification
+                  :marketing_site_updated, :ps4_notification, :student_sub_form,
+                  :subscription_status, :system_of_origin, :updated,
+                  :university_name, :xbox_notification
 
     def initialize(email_address, external_id = nil, first_name = nil, last_name = nil)
       @email = email_address
       @updated = Time.now.iso8601
-      @newsletter_updated = Time.now.iso8601
+      @marketing_site_updated = Time.now.iso8601
       @first_name = first_name unless first_name.to_s.empty?
       @last_name = last_name unless last_name.to_s.empty?
 
